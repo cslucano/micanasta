80c48738568f0e0eeac56cdbe5bb821057d71921
Carlos Salvatierra <cslucano@gmail.com>
2013-09-22 08:19:12 -0500

    actualizamos imagenes de productos

--- migracion/integracion.sh
+++ migracion/integracion.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-cat categoria.sql producto.sql estadistica.sql | psql -h localhost -U micanasta -W micanasta 
+cat categoria.sql producto.sql estadistica.sql updateimage.sql | psql -h localhost -U micanasta -W micanasta 
