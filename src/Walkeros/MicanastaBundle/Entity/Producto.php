e1e5fc15ec130932ecb6cee661a4d72498568c3f
Carlos Salvatierra <cslucano@gmail.com>
2013-09-21 23:04:28 -0500

    agregamos imagenes a los productos

--- src/Walkeros/MicanastaBundle/Entity/Producto.php
+++ src/Walkeros/MicanastaBundle/Entity/Producto.php
@@ -70,6 +70,39 @@ class Producto
      */
     private $giro;
 
+	/**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    public $path;
+
+    public function getAbsolutePath()
+    {
+        return null === $this->path
+            ? null
+            : $this->getUploadRootDir().'/'.$this->path;
+    }
+
+    public function getWebPath()
+    {
+        return null === $this->path
+            ? null
+            : $this->getUploadDir().'/'.$this->path;
+    }
+
+    protected function getUploadRootDir()
+    {
+        // the absolute directory path where uploaded
+        // documents should be saved
+        return __DIR__.'/../../../../web/'.$this->getUploadDir();
+    }
+
+    protected function getUploadDir()
+    {
+        // get rid of the __DIR__ so it doesn't screw up
+        // when displaying uploaded doc/image in the view.
+        return 'images/products';
+    }
+
     /**
      * Get id
      *
